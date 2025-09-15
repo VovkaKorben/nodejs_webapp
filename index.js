e3b620e596912843dd23f9c53390bb636829dc88
VovkaKorben <vovakorben@gmail.com>
2025-09-15 12:49:19 +0300

    A

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const http = require('http');
 
-
+const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Moikka Maailma!!!!\n');
 });
 
-PORT_NUMBER = 8080;
+// PORT_NUMBER = 8080;
 IP_ADDR = '0.0.0.0'
-server.listen(PORT_NUMBER, IP_ADDR, () => {
-    console.log(`Server running at http://${IP_ADDR}:${PORT_NUMBER}/`);
+server.listen(PORT, IP_ADDR, () => {
+    console.log(`Server running at http://${IP_ADDR}:${PORT}/`);
 });
